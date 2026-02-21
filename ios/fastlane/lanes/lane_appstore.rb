81092031cf639ffdf055b4291c7d850f421e8d31
Tommi Niemi <tommi@rotko.net>
2026-02-21 12:31:31 +0700

    fix ios ci: use key_filepath for asc api key to avoid openssl curve error

--- ios/fastlane/lanes/lane_appstore.rb
+++ ios/fastlane/lanes/lane_appstore.rb
@@ -1,10 +1,16 @@
 desc "Load ASC API Key information to use in subsequent lanes"
 lane :load_asc_api_key do
+  require 'base64'
+  require 'tempfile'
+
+  key_file = Tempfile.new(['asc_key', '.p8'])
+  key_file.write(Base64.decode64(ENV["ASC_KEY_BASE64"]))
+  key_file.close
+
   app_store_connect_api_key(
     key_id: ENV["ASC_KEY_ID"],
     issuer_id: ENV["ASC_ISSUER_ID"],
-    key_content: ENV["ASC_KEY_BASE64"],
-    is_key_content_base64: true,
+    key_filepath: key_file.path,
     in_house: false
-   )
+  )
 end
